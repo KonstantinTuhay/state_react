086bb3255c88c6a004185b475601b74378b4ad82
Konstantin Tuhay <tugay-k@mail.ru>
2024-04-14 11:47:43 +0300

    practice with state

--- src/App.js
+++ src/App.js
@@ -1,25 +1,75 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+// 4.
+// const Example = () => {
+//   const [color, setColor] = useState("green");
 
-export default App;
+//   const changeColor = () => {
+//     setColor(color === "green" ? "red" : "green");
+//   };
+
+//   const style = {
+//     color: color,
+//   };
+
+//   return (
+//     <>
+//       <p style={style}>dssfjsdfk dfsjfjdsjfdsjf</p>
+//       <button onClick={changeColor}>click</button>
+//     </>
+//   );
+// };
+
+// 3.
+// const Inp = () => {
+//   const [txt, setTxt] = useState("");
+
+//   const handleChange = (event) => {
+//     setTxt(event.target.value);
+//   };
+
+//   return (
+//     <div>
+//       <label>
+//         <input type="text" value={txt} onChange={handleChange} />
+//       </label>
+//       <p>{txt}</p>
+//     </div>
+//   );
+// };
+
+// 2.
+// const Text = () => {
+//   const [hide, setHide] = useState(true);
+
+//   const hideText = () => {
+//     setHide(!hide);
+//   };
+
+//   return (
+//     <>
+//       <button onClick={hideText}>Hide me!</button>
+//       {hide && <p>Something ....</p>}
+//     </>
+//   );
+// };
+
+// 1
+// const Example = () => {
+//   const [count, setCount] = useState(0);
+//   let increment;
+//   if (count !== 10) {
+//     increment = () => {
+//       setCount(count + 1);
+//     };
+//   }
+
+//   return (
+//     <>
+//       <p>Значение = {count}</p>
+//       <button onClick={increment}>Увеличь значение</button>
+//     </>
+//   );
+// };
+
+export default Example;
